79f30631012f59618d661cd1b5040c9baa26de5a
Nguyễn Thị Phương Mai <maintpse184343@fpt.edu.vn>
2025-11-04 11:16:48 +0700

    Update TextToSpeechLink.cs

--- Backend/Microservices/Ai.Microservice/src/Domain/Entities/TextToSpeechLink.cs
+++ Backend/Microservices/Ai.Microservice/src/Domain/Entities/TextToSpeechLink.cs
@@ -4,11 +4,13 @@ namespace Domain.Entities;
 
 public class TextToSpeechLink
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
 
     public string UserId { get; set; } = string.Empty;
 
     public string UniqueId { get; set; } = string.Empty;
+
+    public string Link { get; set; } = string.Empty;
 
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 }
