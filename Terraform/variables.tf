a4bbedb159aea50cce5aa64c1a10dea2e771bfd8
Nguyễn Thị Phương Mai <maintpse184343@fpt.edu.vn>
2025-11-08 22:48:45 +0700

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -68,6 +68,19 @@ variable "service_discovery_domain_suffix" {
   default     = "svc"
 }
 
+# SSL/TLS Certificate Variable
+variable "certificate_arn" {
+  description = "ARN of the SSL/TLS certificate from AWS Certificate Manager for HTTPS listener. If not provided, only HTTP listener will be created."
+  type        = string
+  default     = null
+}
+
+variable "enable_https_redirect" {
+  description = "Whether to redirect HTTP traffic to HTTPS. Only applies if certificate_arn is provided."
+  type        = bool
+  default     = true
+}
+
 # Service Definitions Variable
 variable "services" {
   description = "Configuration for each microservice"
