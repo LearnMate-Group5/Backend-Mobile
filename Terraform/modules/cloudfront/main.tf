cbe943f1f44819841a733c8850bdb2dd87c4605b
Nguyễn Thị Phương Mai <maintpse184343@fpt.edu.vn>
2025-11-09 14:08:54 +0700

    Update main.tf

--- Terraform/modules/cloudfront/main.tf
+++ Terraform/modules/cloudfront/main.tf
@@ -1,16 +1,25 @@
 # Origin Request Policy - Forward ALL headers for payment gateway IPN callbacks
 resource "aws_cloudfront_origin_request_policy" "include_cloudfront_headers" {
   name    = "${var.project_name}-cloudfront-headers-policy"
-  comment = "Forward all viewer headers to support payment gateway IPN (MoMo, ZaloPay, etc.)"
+  comment = "Forward all viewer headers + CloudFront headers to support payment gateway IPN (MoMo, ZaloPay, etc.)"
 
   cookies_config {
     cookie_behavior = "all"
   }
 
   headers_config {
-    # Forward ALL viewer headers (including Content-Type, custom headers from payment gateways)
+    # Forward ALL viewer headers + whitelisted CloudFront headers
     # This is required for MoMo/ZaloPay IPN callbacks to work correctly
-    header_behavior = "allViewer"
+    header_behavior = "allViewerAndWhitelistCloudFront"
+    headers {
+      items = [
+        "CloudFront-Forwarded-Proto",
+        "CloudFront-Viewer-Country",
+        "CloudFront-Is-Mobile-Viewer",
+        "CloudFront-Is-Tablet-Viewer",
+        "CloudFront-Is-Desktop-Viewer"
+      ]
+    }
   }
 
   query_strings_config {
@@ -20,7 +29,7 @@ resource "aws_cloudfront_origin_request_policy" "include_cloudfront_headers" {
 
 resource "aws_cloudfront_distribution" "alb_distribution" {
   enabled             = true
-  is_ipv6_enabled     = true
+  is_ipv6_enabled     = false  # Disabled for MoMo compatibility (MoMo only supports IPv4)
   comment             = "CloudFront distribution for ${var.project_name} ALB with HTTPS"
   price_class         = var.price_class
   http_version        = "http2and3"
